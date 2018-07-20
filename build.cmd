d712b6e4f7c13edd1b312786e715e6643a35db98
Alex D <duzhar@googlemail.com>
2018-07-20 20:16:26 +0100

    added build steps for VS2017

---
+++ build.cmd
@@ -0,0 +1,14 @@
+if "%1" NEQ "win64" goto :mingw32
+IF NOT EXIST __build MKDIR __build
+cd __build
+IF NOT EXIST win64 MKDIR win64
+cd win64
+cmake -f ../.. -G "Visual Studio 15 2017 Win64"
+goto :end
+:mingw32
+IF NOT EXIST __build MKDIR __build
+cd __build
+IF NOT EXIST mingw32 MKDIR mingw32
+cmake -f ../.. -G "MinGW32 Makefiles"
+mingw_make.exe
+:end
