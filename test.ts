76210dda801794574836f70073348f0dfde919ed
Alex D <oleksandr.duzhar@betssongroup.com>
2018-07-31 16:37:20 +0100

    before refactoring registering track

--- test.ts
+++ test.ts
@@ -1,10 +1,5 @@
-function f() {                         
-            var a = 10;                         
-            return function g() {               
-                var b = a + 1;                  
-                return b;                       
-            }                                   
-        }                                       
-                                                
-        var g = f();                            
-        console.log(g());
+let a,b;
+a = 1
+b = 10;
+a = a + b;
+console.log(a);
