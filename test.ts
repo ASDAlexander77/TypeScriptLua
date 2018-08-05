445cb923493197a9cdde2f9b378029f81f8bdd0e
Alex D <duzhar@googlemail.com>
2018-08-05 17:56:24 +0100

    progress

--- test.ts
+++ test.ts
@@ -1,13 +1,17 @@
 let deck = {
-  suits: ['hearts', 'spades', 'clubs', 'diamonds'],
-  createCardPicker: function () {
-    return function () {
-      return { suit: this.suits[1] };
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker: function() {
+        return function() {
+            let pickedCard = Math.floor(Math.random() * 52);
+            let pickedSuit = Math.floor(pickedCard / 13);
+
+            return {suit: this.suits[pickedSuit], card: pickedCard % 13};
+        }
     }
-  }
 }
 
 let cardPicker = deck.createCardPicker();
 let pickedCard = cardPicker();
 
-console.log(pickedCard.suit);
+console.log("card: " + pickedCard.card + " of " + pickedCard.suit);
