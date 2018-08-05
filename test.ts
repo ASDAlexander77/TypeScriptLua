c8ebb207461bf1a5ebe5f11506760ed8c4e29cfa
Alex D <duzhar@googlemail.com>
2018-08-05 22:24:15 +0100

    tests fix

--- test.ts
+++ test.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function Person(first, last, age, eyecolor) {
   this.firstName = first;
   this.lastName = last;
@@ -8,22 +7,3 @@ function Person(first, last, age, eyecolor) {
 var myFather = new Person("John", "Doe", 50, "blue");
 var myMother = new Person("Sally", "Rally", 48, "green");
 console.log(myFather.lastName);
-=======
-let deck = {
-    suits: ["hearts", "spades", "clubs", "diamonds"],
-    cards: Array(52),
-    createCardPicker: function() {
-        return function() {
-            let pickedCard = Math.floor(Math.random() * 52);
-            let pickedSuit = Math.floor(pickedCard / 13);
-
-            return {suit: this.suits[pickedSuit], card: pickedCard % 13};
-        }
-    }
-}
-
-let cardPicker = deck.createCardPicker();
-let pickedCard = cardPicker();
-
-console.log("card: " + pickedCard.card + " of " + pickedCard.suit);
->>>>>>> 2642978a0a4b145443358a881c8131ffae08d4f9
