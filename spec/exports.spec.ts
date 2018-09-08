28140ab8d0918f8e8994bd2659362e26da2e1aac
Alex D <duzhar@googlemail.com>
2018-09-08 21:15:32 +0100

    first export test

---
+++ spec/exports.spec.ts
@@ -0,0 +1,19 @@
+import { Run } from '../src/compiler';
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+describe('Export', () => {
+
+    it('Class export', () => expect('false\r\n').to.equals(new Run().test([
+        'export class ZipCodeValidator {            \
+            isAcceptable(s: string): boolean {      \
+                return false;                       \
+            }                                       \
+        }                                           \
+    ',
+    'import { ZipCodeValidator } from "./test1";    \
+    const myValidator = new ZipCodeValidator();     \
+    console.log(myValidator.isAcceptable("test"));  \
+    '])));
+
+});
