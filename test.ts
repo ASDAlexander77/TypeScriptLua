e2e98891148a8879d6ee692ca252458b385cffa4
Alex D <oleksandr.duzhar@betssongroup.com>
2018-09-17 12:41:48 +0100

    progress of fixing super call on method

--- test.ts
+++ test.ts
@@ -3,6 +3,7 @@ class Class1 {
 		return false;
 	}
 }
+
 class Class2 extends Class1 {
 	public method1(): boolean {
 		return super.method1();
@@ -10,7 +11,5 @@ class Class2 extends Class1 {
 
 }
 
-const c1 = new Class1();
-console.log(c1.method1());
 const c2 = new Class2();
 console.log(c2.method1());
