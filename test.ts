36aeae4ac0f80d74cfdebcc4b82eba8ed3c060f1
Alex D <duzhar@googlemail.com>
2018-09-25 20:42:06 +0100

    progress

--- test.ts
+++ test.ts
@@ -1,34 +1,7 @@
 function f() {
     switch (returnData.taskType) {
-        case WorkerTaskType.INIT:
-            this._init = true;
-            this._scene.meshes.forEach((mesh) => {
-                this.onMeshAdded(mesh);
-            });
-
-            this._scene.getGeometries().forEach((geometry) => {
-                this.onGeometryAdded(geometry);
-            });
-
-            break;
-        case WorkerTaskType.UPDATE:
-            this._runningUpdated--;
-            break;
         case WorkerTaskType.COLLIDE:
-            var returnPayload: CollisionReplyPayload = returnData.payload;
-            if (!this._collisionsCallbackArray[returnPayload.collisionId]) return;
-
-            let callback = this._collisionsCallbackArray[returnPayload.collisionId];
-
-            if (callback) {
-                let mesh = this._scene.getMeshByUniqueID(returnPayload.collidedMeshUniqueId);
-
-                if (mesh) {
-                    callback(returnPayload.collisionId, Vector3.FromArray(returnPayload.newPosition), mesh);
-                }
-            }
-
-            this._collisionsCallbackArray[returnPayload.collisionId] = null;
+            let mesh = this._scene.getMeshByUniqueID(returnPayload.collidedMeshUniqueId);
             break;
     }
 }
