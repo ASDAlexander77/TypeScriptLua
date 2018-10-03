ec60335b6feddd75528320da737c3909fc216092
Alex D <oleksandr.duzhar@betssongroup.com>
2018-10-03 14:39:14 +0100

    progress of tests

--- test.ts
+++ test.ts
@@ -1,1 +1,18 @@
-module.getX.bind(this);
+class Node {
+    constructor() {
+    }
+}
+
+class Test extends Node {
+    public metadata: any = null;
+
+    constructor() {
+        super();
+    }
+
+    public get x() {
+        return 1;
+    }
+}
+
+new Test();
