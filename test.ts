3b26a68cccf070704229063862e6dd5dc86279a9
Alex D <duzhar@googlemail.com>
2018-10-04 18:34:20 +0100

    found case with error

--- test.ts
+++ test.ts
@@ -1,5 +1,50 @@
-const l = new Array<any>();
+class Node1 {
+    public _scene: Scene;
 
-console.log(l.length);
-l.push(1);
-console.log(l.length);
+    constructor(scene: Scene) {
+        this._scene = scene;
+    }
+
+    public getScene(): Scene {
+        return this._scene;
+    }
+
+    public get parent(): any {
+        return 1;
+    }
+
+    public set parent(v) {
+    }
+}
+
+abstract class AbstractScene {
+}
+
+class Scene extends AbstractScene {
+    private cameras = new Array<Camera>();
+
+    public addCamera(newCamera: Camera): void {
+        this.cameras[1] = newCamera;
+    }
+
+    public get parent(): any {
+        return 1;
+    }
+
+    public set parent(v) {
+    }
+}
+
+class Camera extends Node1 {
+    constructor(scene: Scene) {
+        super(scene);
+        this.getScene().addCamera(this);
+    }
+
+    public get parent(): any {
+        return 1;
+    }
+}
+
+let s = new Scene();
+let c = new Camera(s);
