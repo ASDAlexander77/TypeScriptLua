0fc8eed848e3800eaa1b49ef572ee379290d1f4b
Alex D <oleksandr.duzhar@betssongroup.com>
2018-10-10 16:36:04 +0100

    progress of experimenting

---
+++ vscode-lua-debug/test/shell.ts
@@ -0,0 +1,43 @@
+import { readFileSync } from 'fs';
+import { EventEmitter } from 'events';
+import { spawn, ChildProcess } from 'child_process';
+import { Writable, Readable } from 'stream';
+
+async function f() {
+    console.log('start...');
+
+    const exe = spawn('lua', ['-i']);
+    // const exe = spawn('dir', ['*.*'], { shell: true });
+
+    let line;
+    while (line = await readOutput(exe.stdout)) {
+        console.log(line);
+    }
+
+
+    /*
+    exe.stdout.on('data', (data) => {
+        const text = data.toString();
+        console.log(text);
+    });
+
+    exe.stderr.on('data', (data) => {
+        console.log(data.toString());
+    });
+
+    exe.on('exit', (code) => {
+        console.log(`process exited with code ${code}`);
+    });
+    */
+
+    console.log('end...');
+}
+
+async function readOutput(output: Readable) {
+    return new Promise((resolve, reject) => {
+        output.on('data', (data) => resolve(data));
+        output.on('error', (data) => resolve(undefined));
+    });
+}
+
+f();
