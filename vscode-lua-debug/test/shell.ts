6ea6fdee1f688203d695761ce837047b4fed84b2
Alex D <duzhar@googlemail.com>
2018-10-10 21:54:22 +0100

    progress of experiment

--- vscode-lua-debug/test/shell.ts
+++ vscode-lua-debug/test/shell.ts
@@ -7,39 +7,39 @@ async function f() {
     console.log('start...');
 
     const exe = spawn('lua', ['-i']);
-    // const exe = spawn('dir', ['*.*'], { shell: true });
 
-    exe.stdout.setEncoding('utf8');
-
-    let line;
-    while (line = await readOutput(exe.stdout)) {
-        console.log(line);
-    }
-
-
-    /*
-    exe.stdout.on('data', (data) => {
-        const text = data.toString();
-        console.log(text);
+    exe.on('close', (code) => {
+        console.log(`process exited with code ${code}`);
     });
 
     exe.stderr.on('data', (data) => {
         console.log(data.toString());
     });
 
-    exe.on('exit', (code) => {
-        console.log(`process exited with code ${code}`);
-    });
-    */
+    exe.stdout.setEncoding('utf8');
+    try {
+        let line;
+        while (line = await readOutput(exe.stdout, 3000)) {
+            console.log(line);
+        }
+    } catch (e) {
+        console.error(e);
+    }
+
+    exe.kill();
 
     console.log('end...');
 }
 
-async function readOutput(output: Readable) {
+async function readOutput(output: Readable, timeout?: number) {
     return new Promise((resolve, reject) => {
         output.on('data', (data) => resolve(data));
         output.on('error', () => resolve(undefined));
+        if (timeout) {
+            setTimeout(() => reject(new Error('timeout')), timeout);
+        }
     });
 }
 
 f();
+
