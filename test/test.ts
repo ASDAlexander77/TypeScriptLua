05ee27be4617aeca6c22631496d11cc5dea98d9e
Alex D <oleksandr.duzhar@betssongroup.com>
2018-11-15 13:59:31 +0000

    new test

--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,36 @@
-pause();
-const a = {};
-a["pointer"] = "test";
-for (const k in a)
-{
-	console.log(k, a[k]);
+declare var print;
+
+function fnc(d) {
+    d[0] = 1;
+    d[1] = 2;
+    d['end'] = 'hi';
 }
+
+print('start.');
+
+const s = 'Hello String';
+const f = 3.14;
+const i = 1;
+
+const a = {};
+if (a) {
+    const b = {};
+    a['object b'] = b;
+    a['b'] = b;
+    a[0] = b;
+    a['value'] = 1;
+
+    b[0] = 1;
+    b[1] = 2;
+    b['end'] = 'hi';
+
+    a['end'] = 'end';
+
+    fnc(a);
+}
+
+if (a) {
+    const c = {};
+}
+
+print('done.');
