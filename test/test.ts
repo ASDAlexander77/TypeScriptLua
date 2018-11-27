30d294163cedf889e0869804dbfccbd88ac54c0f
Alex D <oleksandr.duzhar@betssongroup.com>
2018-11-27 09:54:34 +0000

    changes for regex

--- test/test.ts
+++ test/test.ts
@@ -1,37 +1,4 @@
-const p = print
-
-function fnc(d) {
-	const xx = 10;
-    d[0] = 1;
-    d[1] = 2;
-    d["end"] = "hi";
-}
-
-p('start.');
-
-const s = "Hello String";
-const f = 3.14;
-const i = 1;
-
-const a = {};
-if (a) {
-    const b = {};
-    a["object b"] = b;
-    a["b"] = b;
-    a[0] = b;
-    a["value"] = 1;
-
-    b[0] = 1;
-    b[1] = 2;
-    b["end"] = "hi";
-
-    a["end"] = "end";
-
-    fnc(a);
-}
-
-if (a) {
-    const c = {};
-}
-
-p('done.');
+let navigator1 = 'iPad iPhone';
+const _badOS = /iPad/i.test(navigator1);
+const b = _badOS ? 1 : 0;
+console.log(b);
