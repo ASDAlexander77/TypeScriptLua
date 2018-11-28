0f741b1c889fac18e8f9f4c2f13e0d2507a0c4e8
Alex D <oleksandr.duzhar@betssongroup.com>
2018-11-28 10:55:46 +0000

    added test

--- spec/basic_types.spec.ts
+++ spec/basic_types.spec.ts
@@ -79,11 +79,15 @@ I\'ll be 38 years old next month.\r\n').to.equals(new Run().test([
     '])));
 
     it('Const Array with Const Objects', () => expect(new Run().test([
-        'let Colors = [{Red: 1, Green: 2, Blue: 3}, {Red: 4, Green: 5, Blue: 6}];    \
-        let color = Colors[1].Green;                                                 \
-        console.log(color);                                                          \
-        let color1 = Colors[0].Green;                                                \
-        console.log(color1);                                                         \
+        'function f(Colors: any) {                                                    \
+            let color = Colors[1].Green;                                              \
+            console.log(color);                                                       \
+            let color1 = Colors[0].Green;                                             \
+            console.log(color1);                                                      \
+        }                                                                             \
+        let s = { s: "5" };                                                           \
+        let Colors = [{Red: 1, Green: "2", Blue: 3}, {Red: 4, Green: s.s, Blue: 6}];  \
+        f(Colors);                                                                    \
     '])).to.equals('5\r\n2\r\n'));
 
 });
