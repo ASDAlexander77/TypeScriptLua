fa70d91d7a910340a2fcf8579d75114d317ae22e
Alex D <oleksandr.duzhar@betssongroup.com>
2018-12-11 11:58:38 +0000

    progress of debugging

--- spec/class.spec.ts
+++ spec/class.spec.ts
@@ -880,25 +880,73 @@ describe('Classes', () => {
     '])));
 
     it('Class - BUG (base class has setter and derived not)',  () => expect('Run\r\n').to.equals(new Run().test([
-        'class Node1 {                                      \
-        public _scene: Scene;                               \
-                                                            \
-        constructor(scene: Scene) {                         \
-            this._scene = scene;                            \
-        }                                                   \
-                                                            \
-        public set parent(v) {                              \
-        }                                                   \
-    }                                                       \
-                                                            \
-    class Camera extends Node1 {                            \
-        constructor(scene: Scene) {                         \
-            super(null);                                    \
-        }                                                   \
-    }                                                       \
-                                                            \
-    let c = new Camera(s);                                  \
-    console.log("Run");                                     \
+        'class Node1 {                                          \
+            public _scene: Scene;                               \
+                                                                \
+            constructor(scene: Scene) {                         \
+                this._scene = scene;                            \
+            }                                                   \
+                                                                \
+            public set parent(v) {                              \
+            }                                                   \
+        }                                                       \
+                                                                \
+        class Camera extends Node1 {                            \
+            constructor(scene: Scene) {                         \
+                super(null);                                    \
+            }                                                   \
+        }                                                       \
+                                                                \
+        let c = new Camera(s);                                  \
+        console.log("Run");                                     \
     '])));
+
+    it('Class - BUG (chain call)',  () => expect(new Run().test([
+        'class Vector3 {                                        \
+            constructor(                                        \
+                public x: number = 0,                           \
+                public y: number = 0,                           \
+                public z: number = 0                            \
+            ) {                                                 \
+            }                                                   \
+                                                                \
+            public addInPlace(otherVector: Vector3): Vector3 {  \
+                this.x += otherVector.x;                        \
+                this.y += otherVector.y;                        \
+                this.z += otherVector.z;                        \
+                return this;                                    \
+            }                                                   \
+                                                                \
+            public scaleInPlace(scale: number): Vector3 {       \
+                this.x *= scale;                                \
+                this.y *= scale;                                \
+                this.z *= scale;                                \
+                return this;                                    \
+            }                                                   \
+                                                                \
+            public copyFrom(source: Vector3): Vector3 {         \
+                this.x = source.x;                              \
+                this.y = source.y;                              \
+                this.z = source.z;                              \
+                return this;                                    \
+            }                                                   \
+                                                                \
+            public get isNonUniform(): boolean {                \
+                let absX = Math.abs(this.x);                    \
+                let absY = Math.abs(this.y);                    \
+                if (absX !== absY) {                            \
+                    return true;                                \
+                }                                               \
+                                                                \
+                return false;                                   \
+            }                                                   \
+        }                                                       \
+                                                                \
+        const center = new Vector3();                           \
+        const maximum = new Vector3();                          \
+        const minimum = new Vector3();                          \
+        center.copyFrom(maximum).addInPlace(minimum).scaleInPlace(0.5); \
+        console.log("Run1");                                     \
+    '])).to.equals('Run1\r\n'));
 
 });
--- test/test.ts
+++ test/test.ts
@@ -1,2 +1,45 @@
-const x = 1, y = 2;
-console.log(1 && y);
+class Vector3 {
+    constructor(
+        public x: number = 0,
+        public y: number = 0,
+        public z: number = 0
+    ) {
+    }
+
+    public addInPlace(otherVector: Vector3): Vector3 {
+        this.x += otherVector.x;
+        this.y += otherVector.y;
+        this.z += otherVector.z;
+        return this;
+    }
+
+    public scaleInPlace(scale: number): Vector3 {
+        this.x *= scale;
+        this.y *= scale;
+        this.z *= scale;
+        return this;
+    }
+
+    public copyFrom(source: Vector3): Vector3 {
+        this.x = source.x;
+        this.y = source.y;
+        this.z = source.z;
+        return this;
+    }
+
+    public get isNonUniform(): boolean {
+        const absX = Math.abs(this.x);
+        const absY = Math.abs(this.y);
+        if (absX !== absY) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+const center = new Vector3();
+const maximum = new Vector3();
+const minimum = new Vector3();
+center.copyFrom(maximum).addInPlace(minimum).scaleInPlace(0.5);
+console.log('Run1');
