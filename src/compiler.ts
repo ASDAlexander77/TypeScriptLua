8a91e8d2ed05ca40d131f93d4e066aa6f3af2bbf
Alex D <duzhar@googlemail.com>
2018-12-19 11:16:34 +0000

    fix for writing file with lower case

--- src/compiler.ts
+++ src/compiler.ts
@@ -175,7 +175,7 @@ export class Run {
                 emitter.save();
 
                 const fileNamnNoExt = s.fileName.endsWith('.ts') ? s.fileName.substr(0, s.fileName.length - 3) : s.fileName;
-                const fileName = Helpers.correctFileNameForLua(fileNamnNoExt.concat('.', outputExtention));
+                const fileName = Helpers.correctFileNameForLua(fileNamnNoExt.toLowerCase().concat('.', outputExtention));
 
                 console.log('Writing to file ' + fileName);
 
