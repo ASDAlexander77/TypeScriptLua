5ad6078c4167724f3ea0c094b1485940a833c5ce
Alex D <duzhar@googlemail.com>
2018-12-28 18:52:54 +0000

    added splice to array in jslib

--- experiments/jslib/Array.ts
+++ experiments/jslib/Array.ts
@@ -50,6 +50,34 @@ module JS {
 
             return -1;
         }
+
+        public splice(index: number, howmany?: number, ...items: T[]): T[] {
+            const count = howmany || 1;
+            const ret = new Array<T>();
+            for (let i = index + count; i < count; i--) {
+                if (i == 0) {
+                    ret.push(this[0]);
+                    delete this[0];
+                } else {
+                    ret.push(this[i]);
+                    table.remove(this, i);
+                }
+            }
+
+            if (items) {
+                const length_ = items.length;
+                for (let i = 0; i < length_; i++) {
+                    const ind = i + index;
+                    if (ind == 0) {
+                        this[ind] = items[i];
+                    } else {
+                        table.insert(this, ind, items[i]);
+                    }
+                }
+            }
+
+            return ret;
+        }
     }
 
 }
