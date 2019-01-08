2d6ea2fe309a93053ffd01157bbb892f37f2a510
Alex D <oleksandr.duzhar@betssongroup.com>
2019-01-08 15:13:44 +0000

    more refactoring

--- src/preprocessor.ts
+++ src/preprocessor.ts
@@ -67,67 +67,18 @@ export class Preprocessor {
     }
 
     private preprocessCallExpression(callExpression: ts.CallExpression): ts.Expression {
-        // BIND
-        // convert <xxx>.bind(this) into __bind(<xxx>, this, ...);
-        // check if end propertyaccess is 'bind'
-        let memberAccess = callExpression.expression;
-        if (memberAccess.kind === ts.SyntaxKind.PropertyAccessExpression) {
-            const propertyAccessExpression = <ts.PropertyAccessExpression>memberAccess;
-            if (propertyAccessExpression.name.text === 'bind'
-                && this.typeInfo.isResultNonStaticMethodReference(propertyAccessExpression.expression)) {
-                const methodBindCall = ts.createCall(
-                    ts.createIdentifier('__bind'),
-                    undefined,
-                    [ propertyAccessExpression.expression, ...callExpression.arguments ]);
-                // do not use METHOD as parent, otherwise processCallExpression will mess up with return pareneters
-                methodBindCall.parent = propertyAccessExpression.parent.parent;
-                (<any>methodBindCall).__bind_call = true;
-                return methodBindCall;
-            }
+        if (callExpression.expression.kind === ts.SyntaxKind.PropertyAccessExpression) {
 
-            // STRING & NUMBER
-            // string "...".<function>()  => new String("...").<function>()
-            let isConstString = propertyAccessExpression.expression.kind === ts.SyntaxKind.StringLiteral;
-            let isConstNumber = propertyAccessExpression.expression.kind === ts.SyntaxKind.NumericLiteral;
-            if (!isConstString && !isConstNumber) {
-                try {
-                    const typeResult = this.resolver.getTypeAtLocation(propertyAccessExpression.expression);
-                    if (typeResult) {
-                        isConstString = (typeResult.intrinsicName || typeof (typeResult.value)) === 'string';
-                        isConstNumber = (typeResult.intrinsicName || typeof (typeResult.value)) === 'number';
-                    }
-                } catch (e) {
-                    console.warn('Can\'t get type of "' + callExpression.getText() + '"');
-                }
-            }
+            const newExpression =
+                this.preprocessBinding(callExpression)
+                || this.preprocessConstCast(callExpression);
 
-            if (isConstString || isConstNumber) {
-                const methodCall = ts.createCall(
-                    ts.createPropertyAccess(
-                        ts.createIdentifier(
-                            isConstString
-                                ? 'StringHelper'
-                                : (isConstNumber ? 'NumberHelper' : '')),
-                        (<ts.PropertyAccessExpression>memberAccess).name),
-                    undefined,
-                    [(<ts.PropertyAccessExpression>memberAccess).expression, ...callExpression.arguments]);
-                methodCall.parent = callExpression;
-                return methodCall;
+            if (newExpression) {
+                return newExpression;
             }
         }
 
-        // SUPER
-        // convert super.xxx(...) into <Type>.xxx(this, ...);
-        let lastPropertyAccess: ts.PropertyAccessExpression;
-        while (memberAccess.kind === ts.SyntaxKind.PropertyAccessExpression) {
-            lastPropertyAccess = <ts.PropertyAccessExpression>memberAccess;
-            memberAccess = lastPropertyAccess.expression;
-        }
-
-        if (memberAccess.kind === ts.SyntaxKind.SuperKeyword) {
-            // add 'this' parameter
-            callExpression.arguments = <ts.NodeArray<ts.Expression>><any>[ts.createThis(), ...callExpression.arguments];
-        }
+        this.preprocessSuperCalls(callExpression);
 
         return undefined;
     }
@@ -154,11 +105,11 @@ export class Preprocessor {
 
             // in case of getting method
             if ((isRightOfBinaryExpression || isCallParameter || declar)
-                 && this.typeInfo.isResultNonStaticMethodReference(propertyAccessExpression)
-                 && !(<any>propertyAccessExpression).__self_call_required) {
+                && this.typeInfo.isResultNonStaticMethodReference(propertyAccessExpression)
+                && !(<any>propertyAccessExpression).__self_call_required) {
                 // wrap it into method
                 (<any>propertyAccessExpression).__self_call_required = true;
-                const methodWrapCall = ts.createCall(ts.createIdentifier('__wrapper'), undefined, [ propertyAccessExpression ]);
+                const methodWrapCall = ts.createCall(ts.createIdentifier('__wrapper'), undefined, [propertyAccessExpression]);
                 methodWrapCall.parent = propertyAccessExpression.parent;
                 return methodWrapCall;
             } else if (this.typeInfo.isResultFunctioinType(propertyAccessExpression)) {
@@ -195,21 +146,95 @@ export class Preprocessor {
         const decrIndex = ts.createBinary(
             elementAccessExpression.argumentExpression, ts.SyntaxKind.PlusToken, ts.createNumericLiteral('1'));
         const getByteExpr = ts.createCall(ts.createPropertyAccess(stringIdent, byteIdent), undefined,
-        [
-            elementAccessExpression.expression,
-            decrIndex
-        ]);
+            [
+                elementAccessExpression.expression,
+                decrIndex
+            ]);
 
         decrIndex.parent = getByteExpr;
 
         const expr = ts.createCall(
             ts.createPropertyAccess(stringIdent, charIdent),
             undefined,
-            [ getByteExpr ]);
+            [getByteExpr]);
 
         expr.parent = elementAccessExpression.parent;
         getByteExpr.parent = expr;
 
         return expr;
+    }
+
+    // BIND
+    // convert <xxx>.bind(this) into __bind(<xxx>, this, ...);
+    // check if end propertyaccess is 'bind'
+    private preprocessBinding(callExpression: ts.CallExpression): ts.Expression {
+        const propertyAccessExpression = <ts.PropertyAccessExpression>callExpression.expression;
+
+        const isBindCallOfMethod =
+            propertyAccessExpression.name.text === 'bind'
+            && this.typeInfo.isResultNonStaticMethodReference(propertyAccessExpression.expression);
+        if (!isBindCallOfMethod) {
+            return undefined;
+        }
+
+        const methodBindCall = ts.createCall(
+            ts.createIdentifier('__bind'),
+            undefined,
+            [propertyAccessExpression.expression, ...callExpression.arguments]);
+        // do not use METHOD as parent, otherwise processCallExpression will mess up with return pareneters
+        methodBindCall.parent = propertyAccessExpression.parent.parent;
+        (<any>methodBindCall).__bind_call = true;
+        return methodBindCall;
+    }
+
+    private preprocessConstCast(callExpression: ts.CallExpression) {
+        const propertyAccessExpression = <ts.PropertyAccessExpression>callExpression.expression;
+
+        // STRING & NUMBER
+        // string "...".<function>()  => new String("...").<function>()
+        let isConstString = propertyAccessExpression.expression.kind === ts.SyntaxKind.StringLiteral;
+        let isConstNumber = propertyAccessExpression.expression.kind === ts.SyntaxKind.NumericLiteral;
+        if (!isConstString && !isConstNumber) {
+            try {
+                const typeResult = this.resolver.getTypeAtLocation(propertyAccessExpression.expression);
+                if (typeResult) {
+                    isConstString = (typeResult.intrinsicName || typeof (typeResult.value)) === 'string';
+                    isConstNumber = (typeResult.intrinsicName || typeof (typeResult.value)) === 'number';
+                }
+            } catch (e) {
+                console.warn('Can\'t get type of "' + callExpression.getText() + '"');
+            }
+        }
+
+        if (isConstString || isConstNumber) {
+            const methodCall = ts.createCall(
+                ts.createPropertyAccess(
+                    ts.createIdentifier(
+                        isConstString
+                            ? 'StringHelper'
+                            : (isConstNumber ? 'NumberHelper' : '')),
+                            propertyAccessExpression.name),
+                undefined,
+                [propertyAccessExpression.expression, ...callExpression.arguments]);
+            methodCall.parent = callExpression;
+            return methodCall;
+        }
+    }
+
+    private preprocessSuperCalls(callExpression: ts.CallExpression) {
+        let memberAccess = callExpression.expression;
+
+        // SUPER
+        // convert super.xxx(...) into <Type>.xxx(this, ...);
+        let lastPropertyAccess: ts.PropertyAccessExpression;
+        while (memberAccess.kind === ts.SyntaxKind.PropertyAccessExpression) {
+            lastPropertyAccess = <ts.PropertyAccessExpression>memberAccess;
+            memberAccess = lastPropertyAccess.expression;
+        }
+
+        if (memberAccess.kind === ts.SyntaxKind.SuperKeyword) {
+            // add 'this' parameter
+            callExpression.arguments = <ts.NodeArray<ts.Expression>><any>[ts.createThis(), ...callExpression.arguments];
+        }
     }
 }
