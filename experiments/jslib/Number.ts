5c4341dfd18179776d5d035dcbff2737fc311021
Alex D <oleksandr.duzhar@betssongroup.com>
2019-01-10 15:02:36 +0000

    improved number in jslib class

--- experiments/jslib/Number.ts
+++ experiments/jslib/Number.ts
@@ -11,5 +11,18 @@ module TS {
     export class Number {
         public static MAX_VALUE = 1.7976931348623157e+308;
         public static MIN_VALUE = 5e-324;
+
+        private __tostring: () => string;
+
+        public constructor(private constNumber: number) {
+            this.__tostring = function (): string {
+                return tostring(this.constNumber);
+            };
+        }
+
+        public toString(): String {
+            // tslint:disable-next-line:no-construct
+            return new String(tostring(this.constNumber));
+        }
     }
 }
--- test/test.ts
+++ test/test.ts
@@ -1,28 +1,5 @@
 import './JS';
 
-class Test {
-    public run() {
-        const s = '20';
-        this.run2(10, (x) => {
-            this.run3(x, (y) => {
-                this.run4(y);
-                console.log(s);
-            });
-        });
-    }
-
-    public run2(x, callback: (data: any) => void) {
-        callback(x);
-    }
-
-    public run3(x, callback: (data: any) => void) {
-        callback(x);
-    }
-
-    public run4(x) {
-        console.log(x);
-    }
-}
-
-const t = new Test();
-t.run();
+const x = "Hello";
+const y = 20;
+console.log(y);
