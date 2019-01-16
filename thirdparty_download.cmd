cb4906456f4f82bcdb5ad4e234a7ed440f008368
Alex D <oleksandr.duzhar@betssongroup.com>
2019-01-16 09:17:24 +0000

    progress

--- thirdparty_download.cmd
+++ thirdparty_download.cmd
@@ -1,4 +1,6 @@
 git submodule init
 git submodule update
+echo /src > .git\modules\thirdparty\Babylon.js\info\sparse-checkout
 cd thirdparty\Babylon.js
+git config core.sparseCheckout true
 git checkout -b v3.3.0
