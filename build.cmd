47e2c9bcd1d3a6a35db1aaa4852048fe810aa8b9
Alex D <oleksandr.duzhar@betssongroup.com>
2019-01-16 14:26:25 +0000

    added opengl dependencies

--- build.cmd
+++ build.cmd
@@ -1,8 +1,17 @@
 IF NOT EXIST __dist MKDIR __dist
+
+rem Building Lua
 cd thirdparty\lua-build\
 rem build.cmd win64 Release
 @call build.cmd Release
 cd ..\..\..\..
+
+rem Building GLEW
+cd thirdparty\glew-build\
+@call build.cmd Release
+cd ..\..\..\..
+
+rem Building MoonGL
 cd thirdparty\moongl-build\
 rem build.cmd win64 Release
 @call build.cmd Release
---
+++ thirdparty/glew-build/build.cmd
@@ -0,0 +1,21 @@
+IF "%1" EQU "Release" GOTO :set_release
+IF "%2" EQU "Release" GOTO :set_release
+SET Mode=Debug
+GOTO :cont
+:set_release
+SET Mode=Release
+:cont
+IF "%1" NEQ "win64" GOTO :mingw32
+cd ..\glew-2.2.0\build\vc15
+@call "%VS150COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+"%VS150COMNTOOLS%\..\..\MSBuild\15.0\Bin\MSBuild" glew_shared.vcxproj /m:8 /p:Configuration=Release /p:Platform="x64"
+GOTO :end
+:mingw32
+IF NOT EXIST __build MKDIR __build
+cd __build
+IF NOT EXIST mingw32 MKDIR mingw32
+cd mingw32
+cmake -Wno-dev -f ..\..\..\glew-2.2.0\build\cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE="%Mode%"
+@call mingw32-make
+copy bin\glew32.dll ..\..\..\..\__dist
+:end
