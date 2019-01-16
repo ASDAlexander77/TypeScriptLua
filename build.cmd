631c2f831ddae883f6951d8cc327b38cc20e81a1
Alex D <oleksandr.duzhar@betssongroup.com>
2019-01-16 13:11:38 +0000

    added build commands

--- build.cmd
+++ build.cmd
@@ -1,1 +1,9 @@
-thirdparty\lua-build\build.cmd win64 Release
+IF NOT EXIST __dist MKDIR __dist
+cd thirdparty\lua-build\
+rem build.cmd win64 Release
+@call build.cmd Release
+cd ..\..\..\..
+cd thirdparty\moongl-build\
+rem build.cmd win64 Release
+@call build.cmd Release
+cd ..\..\..\..
