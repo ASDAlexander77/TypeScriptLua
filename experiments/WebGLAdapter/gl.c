7fc174180f7494cb15a42569957540b43bd9b7d0
Alex D <oleksandr.duzhar@betssongroup.com>
2019-01-17 17:25:54 +0000

    progress of writing webgl wrapper

--- experiments/WebGLAdapter/gl.c
+++ experiments/WebGLAdapter/gl.c
@@ -10,31 +10,34 @@
 #endif
 
 #ifdef _WIN32
-#    define LIBRARY_API extern __declspec(dllexport)
+#define LIBRARY_API extern __declspec(dllexport)
 #elif
-#    define LIBRARY_API static 
+#define LIBRARY_API static
 #endif
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-	static int createBuffer() {
-		GLuint val;
-		glGenBuffers(1, &val);
-		return val;
-	}
+    static int createBuffer()
+    {
+        GLuint val;
+        glGenBuffers(1, &val);
+        return val;
+    }
 
-	static const struct luaL_Reg webgl[] = {
-		  {"createBuffer", createBuffer},
-		  {NULL, NULL}  /* sentinel */
-	};
+    static const struct luaL_Reg webgl[] = {
+        {"createBuffer", createBuffer},
+        {NULL, NULL} /* sentinel */
+    };
 
-	//name of this function is not flexible
-	LIBRARY_API int luaopen_webgl(lua_State *L) {
-		luaL_newlib(L, webgl);
-		return 1;
-	}
+    //name of this function is not flexible
+    LIBRARY_API int luaopen_webgl(lua_State *L)
+    {
+        luaL_newlib(L, webgl);
+        return 1;
+    }
 
 #ifdef __cplusplus
 }
