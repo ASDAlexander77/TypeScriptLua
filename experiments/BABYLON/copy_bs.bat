06e6009799c016bf8cee48d9c12ce9c64f49bb35
Alex D <oleksandr.duzhar@betssongroup.com>
2019-01-30 10:35:18 +0000

    sync files

--- experiments/BABYLON/copy_bs.bat
+++ experiments/BABYLON/copy_bs.bat
@@ -2,3 +2,7 @@ rmdir /S /Q  BABYLON
 md BABYLON
 xcopy /S ..\..\thirdparty\Babylon.js\src\*.lua .\BABYLON\
 xcopy /S ..\..\thirdparty\Babylon.js\src\*.lua.map .\BABYLON\
+
+
+xcopy /Y ..\jslib\JS.lua .\
+xcopy /Y ..\jslib\JS.lua.map .\
