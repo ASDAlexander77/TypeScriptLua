18863833b8c20f7cf2d5d96ae64febd4c53d7dcf
Alex D <oleksandr.duzhar@betssongroup.com>
2019-01-31 14:16:28 +0000

    added script to copy shaders for open gl test

--- experiments/BABYLON/copy_bs.bat
+++ experiments/BABYLON/copy_bs.bat
@@ -1,6 +1,10 @@
 rmdir /S /Q  BABYLON
 md BABYLON
+echo Copying binary Lua files for BabylonJS
 xcopy /S ..\..\thirdparty\Babylon.js\src\*.lua .\BABYLON\
 xcopy /S ..\..\thirdparty\Babylon.js\src\*.lua.map .\BABYLON\
+
+echo Copying source for Shaders
+xcopy /S ..\..\thirdparty\Babylon.js\src\Shaders .\Shaders\
 
 @call copy_js.bat
