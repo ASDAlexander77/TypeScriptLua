4ca135b9f2d89de15fa12dfc21d5cbb789237f5d
Alex D <oleksandr.duzhar@betssongroup.com>
2019-01-31 14:19:50 +0000

    sync batch files

--- experiments/BABYLON/copy_bs.bat
+++ experiments/BABYLON/copy_bs.bat
@@ -4,6 +4,8 @@ echo Copying binary Lua files for BabylonJS
 xcopy /S ..\..\thirdparty\Babylon.js\src\*.lua .\BABYLON\
 xcopy /S ..\..\thirdparty\Babylon.js\src\*.lua.map .\BABYLON\
 
+rmdir /S /Q  Shaders
+md Shaders
 echo Copying source for Shaders
 xcopy /S ..\..\thirdparty\Babylon.js\src\Shaders .\Shaders\
 
