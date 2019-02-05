2f399ccc97089e206050dd5bc5f055ba3a4f8072
Alex D <oleksandr.duzhar@betssongroup.com>
2019-02-05 10:56:23 +0000

    progress of implementing JSLib

--- experiments/jslib/Common.ts
+++ experiments/jslib/Common.ts
@@ -1,4 +1,33 @@
+declare var math: any;
+declare var tonumber: any;
+
 // dummy function
 function decodeURIComponent(s: string) {
     return s;
+}
+
+function parseInt(v: any) {
+    if (!v) {
+        return math.nan;
+    }
+
+    const num = tonumber(v);
+    if (!num) {
+        return math.nan;
+    }
+
+    return math.floor(num);
+}
+
+function parseFloat(v: any) {
+    if (!v) {
+        return math.nan;
+    }
+
+    const num = tonumber(v);
+    if (!num) {
+        return math.nan;
+    }
+
+    return num;
 }
