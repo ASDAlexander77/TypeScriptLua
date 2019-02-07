65a54cacd7abf9e6273f8c31e619d6c8fe42c003
Alex D <oleksandr.duzhar@betssongroup.com>
2019-02-07 12:00:24 +0000

    fix to build batch file

--- experiments/BABYLON/build_bs.bat
+++ experiments/BABYLON/build_bs.bat
@@ -23,6 +23,7 @@ cd ..\..\..\experiments\BABYLON
 xcopy /S ..\..\thirdparty\Babylon.js\src\*.lua .\BABYLON\
 xcopy /S ..\..\thirdparty\Babylon.js\src\*.lua.map .\BABYLON\
 
+cd
 rmdir /S /Q Shaders
 md Shaders
 echo =
@@ -34,7 +35,13 @@ echo =
 
 xcopy /S /Y ..\..\thirdparty\Babylon.js\src\Shaders .\Shaders\
 
-
 @call copy_js.bat
+
+echo =
+echo ==================================
+echo Building Babylon.JS Client
+echo ==================================
+echo =
+echo =
 
 node ../../__out/main.js
