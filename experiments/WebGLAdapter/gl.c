0fca8138ee2b0ffdad22d506eeea5b763c0a1b08
Alex D <oleksandr.duzhar@betssongroup.com>
2019-02-11 16:05:45 +0000

    added debug output to opengl

--- experiments/WebGLAdapter/gl.c
+++ experiments/WebGLAdapter/gl.c
@@ -92,6 +92,14 @@ extern "C"
         return majorVersion > major || majorVersion == major && minorVersion >= minor;
     }
 
+#if _DEBUG
+    void GLAPIENTRY MessageCallback( GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam )
+    {
+        fprintf( stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+            ( type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "" ), type, severity, message );
+    }
+#endif
+
     static int initGL(lua_State *L)
     {
         GLenum err = glewInit();
@@ -137,6 +145,11 @@ extern "C"
             return error;
         }
 
+#if _DEBUG
+        // During init, enable debug output
+        glEnable              ( GL_DEBUG_OUTPUT );
+        glDebugMessageCallback( MessageCallback, 0 );
+#endif
         return 0;
     }
 
