3b7e5f3fc2ff83033ca794bfa2121da56cfe9a91
Alex D <duzhar@googlemail.com>
2019-03-05 00:09:17 +0000

    found issue if base class is not defined, it may break the app

--- experiments/BABYLON/babyloncli.ts
+++ experiments/BABYLON/babyloncli.ts
@@ -90,8 +90,8 @@ import './BABYLON/Loading/babylon_sceneLoader';
 import './BABYLON/Tools/babylon_database';
 import './BABYLON/Loading/Plugins/babylon_babylonFileLoader';
 
-import './BABYLON/Cameras/babylon_universalCamera';
 import './BABYLON/Cameras/babylon_touchCamera';
+import './BABYLON/Cameras/babylon_universalCamera';
 
 import WindowEx from './window';
 import DocumentEx from './document';
