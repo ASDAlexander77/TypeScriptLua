23a088d73a35cbb3148a02fc88f0e3d7cc45532d
Alex D <oleksandr.duzhar@betssongroup.com>
2019-03-07 15:23:39 +0000

    sync files

--- thirdparty/freeimage-build/build.cmd
+++ thirdparty/freeimage-build/build.cmd
@@ -6,13 +6,9 @@ GOTO :cont
 SET Mode=Release
 :cont
 IF "%1" NEQ "win64" GOTO :mingw32
-IF NOT EXIST __build MKDIR __build
-cd __build
-IF NOT EXIST win64 MKDIR win64
-cd win64
-cmake -Wno-dev -f ..\..\..\freeimage -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=%Mode% -DZLIB_ROOT=..\zlib-build\__build\win64
+cd ..\freeimage-3.18.0
 if not defined DevEnvDir (@call "%VS150COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64)
-"%VS150COMNTOOLS%\..\..\MSBuild\15.0\Bin\MSBuild" ALL_BUILD.vcxproj /m:8 /p:Configuration=%Mode% /p:Platform="x64"
+"%VS150COMNTOOLS%\..\..\MSBuild\15.0\Bin\MSBuild" FreeImage.2017.sln /m:8 /p:Configuration=%Mode% /p:Platform="x64"
 IF "%Mode%" NEQ "Debug" GOTO :skip_debug_copy
 copy bin\%Mode%\freeimaged.dll ..\..\..\..\__dist\freeimaged.dll
 copy bin\%Mode%\freeimaged.dll ..\..\..\..\__dist\freeimage.dll
