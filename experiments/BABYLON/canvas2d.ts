ebdbcec8e5b9fa0f2ed2b170a0b484f18f52d3f6
Alex D <oleksandr.duzhar@betssongroup.com>
2019-03-18 17:10:50 +0000

    progress

--- experiments/BABYLON/canvas2d.ts
+++ experiments/BABYLON/canvas2d.ts
@@ -3,6 +3,8 @@
 // @ts-ignore
 export default class Canvas2d implements CanvasRenderingContext2D {
 
+    private img: ImageObject;
+
     // @ts-ignore
     constructor() {
     }
@@ -10,6 +12,12 @@ export default class Canvas2d implements CanvasRenderingContext2D {
     // @ts-ignore
     public drawImage(image: CanvasImageSource, sx: number, sy: number, sw: number,
         sh: number, dx: number, dy: number, dw: number, dh: number): void {
-            const img = <ImageObject><any>image;
+        this.img = <ImageObject><any>image;
+    }
+
+    public getImageData(sx: number, sy: number, sw: number, sh: number): ImageData {
+        const data = new Uint8Array(0);
+        (<any>data).buffer = (<any>this.img.bits).buffer;
+        return { data: <ImageData><any>data };
     }
 }
