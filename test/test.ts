af2ba1988b83ee5e6aec675d7db82020ef26baa4
Alex D <oleksandr.duzhar@betssongroup.com>
2019-10-24 15:31:34 +0100

    more fixes

--- test/test.ts
+++ test/test.ts
@@ -1,11 +1,20 @@
-        class Class1 {                                     
-            constructor(private i: number) {                
+	class Class1 {                                     
+            public method1(): boolean {                     
+                return false;                               
+            }                                               
+        }                                                   
+        class Class2 extends Class1 {                       
+                                                            
+            public static get Name() {                      
+                return "test";                              
             }                                               
                                                             
-            public show() {                                 
-              print(this.i);                          
+            public method2(): boolean {                     
+                return true;                                
             }                                               
-          }                                                 
-                                                            
-          let c = new Class1(1);                            
-          c.show();                                         
+        }                                                   
+        const c1 = new Class1();                            
+        console.log(c1.method1());                          
+        const c2 = new Class2();                            
+        console.log(c2.method1());                          
+        console.log(c2.method2());                          
