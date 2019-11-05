2e6d01ec7702a0d1334e8ea40f1bde58c2c82fc6
Alex D <oleksandr.duzhar@betssongroup.com>
2019-11-05 09:42:10 +0000

    fixes

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,15 @@
-class StringHelper {
-        public static getLength(this: string): number {
-            return string.len(this);
-        }
-}
+	class Class1 {                                 
+            constructor(v1: number, v2:string, v3: number, d: number = 10) {  
+                console.log(v1);                        
+                console.log(v2);                        
+                console.log(v3);                        
+                console.log(d);                         
+            }                                           
+                                                        
+            public static Identity(): number {          
+                return "class1";                        
+            }                                           
+        }                                               
+                                                        
+        var c = new Class1(1, Class1.Identity(), 3);    
+        c.show();                                       
