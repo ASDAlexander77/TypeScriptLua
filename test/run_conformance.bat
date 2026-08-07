d87f17681905a34d1cfd1bb5516b1a263d277b47
ASDAlexander77 <duzhar@googlemail.com>
2026-08-07 23:01:59 +0100

    fix: update paths in conformance test scripts and configuration

--- test/run_conformance.bat
+++ test/run_conformance.bat
@@ -1,1 +1,1 @@
-FOR %%i IN (..\..\TypeScriptLua\spec\conformance\tests\*.lua) DO ..\__dist\lua.exe -e "require('./JS')" %%i
+FOR %%i IN (..\spec\conformance\tests\*.lua) DO ..\__dist\lua.exe -e "require('./JS')" %%i
